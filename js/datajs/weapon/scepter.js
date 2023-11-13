7f28bc0a12570c24a021c5ed43950459f872b2bd
zhuxing13 <150545583+zhuxing13@users.noreply.github.com>
2023-11-13 10:34:57 +0800

    Add files via upload

---
+++ js/datajs/weapon/scepter.js
@@ -0,0 +1,37 @@
+/* 权杖 */
+
+const scepterWeaponAttributes = {
+    '模版':{
+        roleProperties: '虚',       /* 武器属性 */
+        weaponType: '单手剑',       /* 武器类型 */
+        weaponCoefficient: 3,       /* 武器系数 */
+        crit: 1,                    /* 自带暴击 */
+        addAtk: 1,                  /* 攻击加成 */
+        addDef: 1,                  /* 防御加成 */
+        addHP: 1,                   /* 生命加成 */
+        addHeal: 1,                 /* 回血加成 */
+        basicAtk: 1,                /* 1级攻击白值 */
+        basicDef: 1,                /* 1级防御白值 */
+        basicHeal: 1,               /* 1级回血值白值 */
+        skill: 1,                   /* 技伤 */
+        cdSkill: 1,                 /* cd */
+        reduceDamage: 1,            /* 伤害减免 */
+    },
+    '天使之音':{
+        roleProperties: '虚',    
+        weaponType: '权杖',    
+        weaponCoefficient: 3,    
+        crit: 2,                 
+        addAtk: 10,               
+        addDef: 15,               
+        addHP: 20,                
+        addHeal: 0,              
+        basicAtk: 209,               
+        basicDef: 0,               
+        basicHeal: 0,               
+        skill: 0,              
+        cdSkill: 15,            
+        reduceDamage: 0,         
+    },
+
+}
